5c88e9f7f0155f9cfff8b909df0e24c9f0255758
Lavesh Gaurav <lavesh.g96@gmail.com>
2020-12-09 21:11:57 +0530

    Build

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
+import React from "react";
 import "./App.css";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import Navigation from "./components/navigation/Navigation";
 import HomePage from "./pages/homePage/HomePage";
-import "bootstrap/dist/css/bootstrap.min.css";
+
 function App() {
   return (
-    <Router basename="/">
+    <React.Fragment>
       <Navigation />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-      </Switch>
-    </Router>
+      <HomePage />
+    </React.Fragment>
+    // <Router basename="/">
+    //   <Switch>
+    //     <Route exact path="/" component={HomePage} />
+    //   </Switch>
+    // </Router>
   );
 }
 
